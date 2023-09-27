9e23ba5bf1e5e74e27af9a658210da3cdff9b50f
Divya Sharma <divyaonline20@gmail.com>
2023-09-28 01:44:33 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./index.css";
+import Contact from "./components/Contact";
+import Hero from "./components/Hero";
+import NavBar from "./components/NavBar";
+import MySkills from "./components/MySkills";
+import Work from "./components/Work";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Hero />
+      <MySkills />
+      <Work />
+      <Contact />
     </div>
   );
 }
